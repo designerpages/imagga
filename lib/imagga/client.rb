ecf0e9b03ca645b5aaa3e517276f32a132067393
Robert Jenkins <bobert@gmail.com>
2015-04-06 16:46:35 -0400

    - Imagga::Client.extract_with_category now requires dp_category in options ry

--- lib/imagga/client.rb
+++ lib/imagga/client.rb
@@ -5,7 +5,8 @@ module Imagga
       ExtractResultBuilder.new.build_from(super(options))
     end
 
-    def extract_with_category(urls_or_images, options={:extract_overall_colors => 1, :extract_object_colors =>0, :apply_color_threshold => 1, :classify_with_threshold => 9.6})
+    def extract_with_category(urls_or_images, options={:extract_overall_colors => 1, :extract_object_colors =>0, :apply_color_threshold => 1, :classify_with_threshold => 99.6 })
+      raise "When applying classifier options[:dp_category] can not be blank ie {:dp_category => carpet}" if options[:dp_category].blank?
       options.merge!(ImageOrUrlParametizer.new.parametrize(urls_or_images))
       ExtractResultBuilder.new.build_from(super(options))
     end
